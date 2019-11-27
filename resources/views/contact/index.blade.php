cf3d8f8af8068043c30cd4d21481276dbd302e6d
Javier Lona <javierlona1@gmail.com>
2019-11-27 16:14:19 -0600

    Extended the layout to contact form

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,39 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <title>Contact Form</title>
-</head>
-<body>
-  <div class="container">
-    <form method="POST" action="{{ route('contact') }}">
-      <div class="form-group row">
-        <label for="name" class="col-sm-2 col-form-label">Name</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" name="name" id="name" placeholder="Name">
-        </div>
+@extends('layouts.app')
+
+@section('title')
+  Directory
+@endsection('title')
+
+@section('content')
+  <form method="POST" action="{{ route('contact') }}">
+    <div class="form-group row">
+      <label for="name" class="col-sm-2 col-form-label">Name</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" name="name" id="name" placeholder="Name">
       </div>
-      <div class="form-group row">
-        <label for="email" class="col-sm-2 col-form-label">Email</label>
-        <div class="col-sm-10">
-          <input type="email" class="form-control" name="email" id="email" placeholder="Email">
-        </div>
+    </div>
+    <div class="form-group row">
+      <label for="email" class="col-sm-2 col-form-label">Email</label>
+      <div class="col-sm-10">
+        <input type="email" class="form-control" name="email" id="email" placeholder="Email">
       </div>
-      <div class="form-group row">
-        <label for="message" class="col-sm-2 col-form-label">Message</label>
-        <div class="col-sm-10">
-          <textarea class="form-control" id="message" rows="3" name="message"></textarea>
-        </div>
+    </div>
+    <div class="form-group row">
+      <label for="message" class="col-sm-2 col-form-label">Message</label>
+      <div class="col-sm-10">
+        <textarea class="form-control" id="message" rows="3" name="message"></textarea>
       </div>
-      <div class="form-group row">
-        <div class="col-sm-10">
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
+    </div>
+    <div class="form-group row">
+      <div class="col-sm-10">
+        <button type="submit" class="btn btn-primary">Submit</button>
       </div>
-    </form>
-  </div>
-</body>
-</html>
+    </div>
+  </form>
+@endsection('content')
