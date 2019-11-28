361cb9171efbfe8b9e81df44a2e9eaa5c67b9fcf
Javier Lona <javierlona1@gmail.com>
2019-11-28 15:06:53 -0600

    Ability to add a person added

--- app/Http/Controllers/PersonnelController.php
+++ app/Http/Controllers/PersonnelController.php
@@ -26,9 +26,50 @@ class PersonnelController extends Controller
       return view('profile.index', $person);
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, Personnel $person)
     {
-      // dd($request);
+      // dd($person);
       echo "HELLO";
+      $person->fill($request->input());
+      $person->save();
+    }
+
+    public function store(Request $request)
+    {
+      $rules = [
+        'name_first' => 'required',
+        'name_last' => 'required',
+        'job_title' => 'required',
+        'email' => 'required|email',
+        'phone' => 'regex:/^\d{3}-\d{3}-\d{4}$/',
+        'office_number' => 'required|numeric'
+      ];
+
+      $validator = Validator::make($request->all(), $rules);
+
+      if($validator->fails()) {
+        // back does not work
+        // sessions are not available 'stateless'
+
+        /*
+        * Use the method below to redirect to a
+        * view with message when sessions are not available
+        */
+        $data['errorMsg'] = 'Failed Validation';
+        return view('profile.add', $data);
+      }
+
+      // var_dump($request->all());
+
+      /*
+      * Mass assignment method to save information
+      *
+      */
+      $profile = Personnel::create($request->input());
+
+      /*
+      * Use the method below to redirect to an unamed route
+      */
+      return redirect('/directory');
     }
 }
--- resources/views/profile/add.blade.php
+++ resources/views/profile/add.blade.php
@@ -5,7 +5,11 @@
 @endsection('title')
 
 @section('content')
-
+@if( !empty($errorMsg))
+  <div class="alert alert-danger" role="alert">
+    {{ $errorMsg ?? '' }}
+  </div>
+@endif
 <form method="post" action="{{ route('profile.add') }}">
   @include('partials.profile_form')
   <button type="submit" class="btn btn-primary">Save Changes</button>
